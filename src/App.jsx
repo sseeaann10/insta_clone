149c22da5e96f6106661b9b8078a47797c46bde2
sean <seanivan.ridulme@gmail.com>
2023-11-03 15:02:21 +0100

    navbar set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-function App() {
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Home from "./pages/Home";
+import LeftSideNav from "./components/LeftSideNav";
+
+const App = () => {
   return (
-    <div className="App">
-      
-      <h1>React App</h1>
+    <div>
+      <Router>
+        {/* <Provider store={store}> */}
+        <LeftSideNav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        {/* </Provider> */}
+      </Router>
     </div>
   );
 }
---
+++ src/components/LeftSideNav.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const LeftSideNav = () => {
+    return (
+      <nav>
+        <Link to={"/"}>Home</Link>{" "}
+      </nav>
+    );
+  };
+  
+  export default LeftSideNav;
