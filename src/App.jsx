ddc75bcef071b4302cb403959e6b604d674b8c4b
sean <seanivan.ridulme@gmail.com>
2023-11-03 14:49:47 +0100

    set up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
