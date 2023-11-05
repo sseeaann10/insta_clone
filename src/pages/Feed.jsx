15e2b9e8647bfc8a24267d2aa8bce3b82fae83c0
Sean <seanivan.ridulme@gmail.com>
2023-11-05 17:25:17 +0100

    possibility to add image in post

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -3,22 +3,36 @@ import { useAtom } from "jotai";
 import { postAtom } from "../utils/globalstate";
 import './Feed.css';
 
-function Post({ text }) {
+function Post({ text, image }) {
     return (
         <div className="post">
             <p>{text}</p>
+            {image && <img src={image} alt="post" />}
         </div>
     );
 }
 
+
 function PostForm() {
     const [content, setContent] = useState('');
+    const [image, setImage] = useState(null); // New state for the image
     const [posts, setPosts] = useAtom(postAtom);
 
     const handlePost = () => {
-        if (content.trim()) {
-            setPosts(prevPosts => [...prevPosts, content]);
+        if (content.trim() || image) {
+            const newPost = {
+                text: content,
+                imageUrl: image ? URL.createObjectURL(image) : null
+            };
+            setPosts(prevPosts => [...prevPosts, newPost]);
             setContent('');
+            setImage(null); // Clear the image after posting
+        }
+    };
+
+    const handleImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            setImage(event.target.files[0]);
         }
     };
 
@@ -29,10 +43,16 @@ function PostForm() {
                 value={content}
                 onChange={e => setContent(e.target.value)}
             />
+            <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+            />
             <button onClick={handlePost}>Post</button>
         </div>
     );
 }
+
 
 function Feed() {
     const [posts] = useAtom(postAtom);
@@ -41,12 +61,13 @@ function Feed() {
         <div className="feed">
             <h1>Posts</h1>
             <PostForm />
-            {posts.map((text, index) => (
-                <Post key={index} text={text} />
+            {posts.map((post, index) => (
+                <Post key={index} text={post.text} image={post.imageUrl} />
             ))}
         </div>
     );
 }
+
 
 
 export default Feed;
