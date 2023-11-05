d6e62cca58ebc7e6af279237d2fe7a9052ed1248
Sean <seanivan.ridulme@gmail.com>
2023-11-05 17:37:57 +0100

    possibility to add mp3 in post

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -3,71 +3,84 @@ import { useAtom } from "jotai";
 import { postAtom } from "../utils/globalstate";
 import './Feed.css';
 
-function Post({ text, image }) {
-    return (
-        <div className="post">
-            <p>{text}</p>
-            {image && <img src={image} alt="post" />}
-        </div>
-    );
+function Post({ text, image, audio }) {
+  return (
+    <div className="post">
+      <p>{text}</p>
+      {image && <img src={image} alt="Post" />}
+      {audio && <audio controls src={audio}>
+                  Your browser does not support the audio element.
+                </audio>}
+    </div>
+  );
 }
-
 
 function PostForm() {
-    const [content, setContent] = useState('');
-    const [image, setImage] = useState(null); // New state for the image
-    const [posts, setPosts] = useAtom(postAtom);
+  const [content, setContent] = useState('');
+  const [image, setImage] = useState(null);
+  const [audio, setAudio] = useState(null);
+  const [posts, setPosts] = useAtom(postAtom);
 
-    const handlePost = () => {
-        if (content.trim() || image) {
-            const newPost = {
-                text: content,
-                imageUrl: image ? URL.createObjectURL(image) : null
-            };
-            setPosts(prevPosts => [...prevPosts, newPost]);
-            setContent('');
-            setImage(null); // Clear the image after posting
-        }
-    };
+  const handlePost = () => {
+    if (content.trim() || image || audio) {
+      const newPost = {
+        text: content,
+        imageUrl: image ? URL.createObjectURL(image) : null,
+        audioUrl: audio ? URL.createObjectURL(audio) : null,
+      };
+      setPosts(prevPosts => [...prevPosts, newPost]);
+      setContent('');
+      setImage(null);
+      setAudio(null);
+    }
+  };
 
-    const handleImageChange = (event) => {
-        if (event.target.files && event.target.files[0]) {
-            setImage(event.target.files[0]);
-        }
-    };
+  const handleImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setImage(event.target.files[0]);
+    }
+  };
 
-    return (
-        <div className="post-form">
-            <textarea
-                placeholder="What's on your mind?"
-                value={content}
-                onChange={e => setContent(e.target.value)}
-            />
-            <input
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-            />
-            <button onClick={handlePost}>Post</button>
-        </div>
-    );
+  const handleAudioChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setAudio(event.target.files[0]);
+    }
+  };
+
+  return (
+    <div className="post-form">
+      <textarea
+        placeholder="What's on your mind?"
+        value={content}
+        onChange={e => setContent(e.target.value)}
+      />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageChange}
+      />Image
+      <input
+        type="file"
+        accept="audio/*"
+        onChange={handleAudioChange}
+      />Audio
+      <button onClick={handlePost}>Post</button>
+    </div>
+  );
 }
-
 
 function Feed() {
-    const [posts] = useAtom(postAtom);
+  const [posts] = useAtom(postAtom);
 
-    return (
-        <div className="feed">
-            <h1>Posts</h1>
-            <PostForm />
-            {posts.map((post, index) => (
-                <Post key={index} text={post.text} image={post.imageUrl} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="feed">
+      <h1>Posts</h1>
+      <PostForm />
+      {posts.map((post, index) => (
+        <Post key={index} text={post.text} image={post.imageUrl} audio={post.audioUrl} />
+      ))}
+    </div>
+  );
 }
-
-
 
 export default Feed;
